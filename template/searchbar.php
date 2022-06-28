1ff094ebc853f8551b9ce0c0dcf78022f02913ea
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-06-28 12:42:57 -0300

    searchbar css

--- template/searchbar.php
+++ template/searchbar.php
@@ -1,23 +1,68 @@
 <div class="search-bar">
-    <div class="section">
-        <div class="section-title">
-            <img src="/res/icon/icon.svg" class="icon">
-            <p style="height: 24px;">Destino</p>
+    <div class="section-wrapper">
+        <div class="section">
+            <div class="section-title">
+                <img src="/res/icon/icon.svg" class="icon">
+                <p style="height: 24px;">Destino</p>
+            </div>
+            
+            <input 
+                type="text"
+                class="section-data" 
+                value="São Paulo"
+            />
         </div>
+    </div>
+    
+    
+    <div class="section-wrapper">
+        <div class="separator"></div>
+
+        <div class="section">
+            <div class="section-title">
+                <img src="/res/icon/calendar.svg" class="icon">
+                <p style="height: 24px;">Entrada</p>
+            </div>
+            
+            <p class="section-data" style="height: 26px;">
+                22/10/2022
+            </p>
+        </div>
+
+    </div>
+    
+    
+    <div class="section-wrapper">
+        <div class="separator"></div>
+
+        <div class="section">
+            <div class="section-title">
+                <img src="/res/icon/calendar.svg" class="icon">
+                <p style="height: 24px;">Saída</p>
+            </div>
+            
+            <p class="section-data" style="height: 26px;">
+                22/10/2022
+            </p>
+        </div>
+    </div>
+    
+    <div class="section-wrapper">
+        <div class="separator"></div>
         
-        <p class="section-data" style="height: 26px;">
-            São Paulo
-        </p>
+        <div class="section">
+            <div class="section-title">
+                <img src="/res/icon/guests.svg" class="icon">
+                <p style="height: 24px;">Hospedes</p>
+            </div>
+            
+            <p class="section-data" style="height: 26px;">
+                2 Adulto(s), 0 Criança(s)
+            </p>
+        </div>
     </div>
 
-    <div class="section">
-        <div class="section-title">
-            <img src="/res/icon/calendar.svg" class="icon">
-            <p style="height: 24px;">Entrada</p>
-        </div>
-        
-        <p class="section-data" style="height: 26px;">
-            22/10/2022
-        </p>
-    </div>
+    <button class="btn-pesquisar">
+        Pesquisar
+    </button>
 </div>
